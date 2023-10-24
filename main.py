2005087d9e803aa77b8a1eca149f1cc52421575f
MythMoth <jonomunro@gmail.com>
2023-10-24 08:38:17 +0200

    First Commmit

---
+++ main.py
@@ -0,0 +1,8 @@
+# User Inputs
+print("Welcome to the Band Name Generator.")
+cityName = input("What's the name of the city you grew up in?\n")
+petName = input("What's your pet's name?\n")
+
+# Band-Name Calculations and Output
+bandName = cityName + " " + petName
+print("Your Band Name could be: " + bandName)
